ceca56cbe7d3d58062aa50ebe3e3eb2cac7ec774
Luis Sanchez <lalcubo@gmail.com>
2022-04-04 17:08:45 -0400

    tablas padres numeros 2,4,6,8

---
+++ app/Models/marca_vehi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class marca_vehi extends Model
+{
+    protected $table = "marca_vehi";
+    use HasFactory;
+}
---
+++ database/migrations/2022_04_04_191439_create_personas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id();
+            $table->string('pers_cedula', 10)->unique();
+            $table->string('pers_nombres', 100);
+            $table->string('pers_apellidos', 100);
+            $table->string('pers_telefono', 14);
+            $table->string('pers_correo', 100);
+            $table->string('pers_estado', 1);
+            $table->string('pers_observacion', 250);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personas');
+    }
+}
