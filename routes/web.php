374fe452a44b43d741c9addc0a03cbbe8d5fb66a
Luis Sanchez <lalcubo@gmail.com>
2022-08-05 16:29:46 -0400

    se acomodo los permisos en web.php

--- routes/web.php
+++ routes/web.php
@@ -29,33 +29,33 @@ Route::delete('/roles/{role}', 'App\Http\Controllers\Admin\RoleController@destro
 Route::get('/roles/{role}/edit', 'App\Http\Controllers\Admin\RoleController@edit')->middleware('can:admin.role.edit');
 Route::put('/roles/{role}', 'App\Http\Controllers\Admin\RoleController@update')->middleware('can:admin.role.edit');
 
-Route::get('/institu', 'App\Http\Controllers\Usuario\InstitucionController@index')->middleware('can:admin.role.index');
-Route::post('/instituregist', 'App\Http\Controllers\Usuario\InstitucionController@store')->middleware('can:admin.role.create');
-//Route::delete('/institu/{id}', 'App\Http\Controllers\Usuario\InstitucionController@destroy')->middleware('can:admin.role.destroy');
-Route::get('/institu/{id}/edit', 'App\Http\Controllers\Usuario\InstitucionController@edit')->middleware('can:admin.role.edit');
-Route::put('/institu/{id}/{acti?}', 'App\Http\Controllers\Usuario\InstitucionController@update')->middleware('can:admin.role.edit');
+Route::get('/institu', 'App\Http\Controllers\Usuario\InstitucionController@index')->middleware('can:insti.user.index');
+Route::post('/instituregist', 'App\Http\Controllers\Usuario\InstitucionController@store')->middleware('can:insti.user.create');
+//Route::delete('/institu/{id}', 'App\Http\Controllers\Usuario\InstitucionController@destroy')->middleware('can:insti.user.destroy');
+Route::get('/institu/{id}/edit', 'App\Http\Controllers\Usuario\InstitucionController@edit')->middleware('can:insti.user.edit');
+Route::put('/institu/{id}/{acti?}', 'App\Http\Controllers\Usuario\InstitucionController@update')->middleware('can:insti.user.desactivar');
 
-Route::get('/programa', 'App\Http\Controllers\Usuario\ProgramacionController@index')->middleware('can:admin.role.index');
-Route::post('/programaregist', 'App\Http\Controllers\Usuario\ProgramacionController@store')->middleware('can:admin.role.create');
-//Route::delete('/programa/{id}', 'App\Http\Controllers\Usuario\ProgramacionController@destroy')->middleware('can:admin.role.destroy');
-Route::get('/programa/{flo?}/', 'App\Http\Controllers\Usuario\ProgramacionController@index')->middleware('can:admin.role.edit');
-Route::get('/programa/{id}/edit', 'App\Http\Controllers\Usuario\ProgramacionController@edit')->middleware('can:admin.role.edit');
-Route::put('/programa/{progra}/{acti?}', 'App\Http\Controllers\Usuario\ProgramacionController@update')->middleware('can:admin.role.edit');
+Route::get('/programa', 'App\Http\Controllers\Usuario\ProgramacionController@index')->middleware('can:program.user.index');
+Route::post('/programaregist', 'App\Http\Controllers\Usuario\ProgramacionController@store')->middleware('can:program.user.create');
+//Route::delete('/programa/{id}', 'App\Http\Controllers\Usuario\ProgramacionController@destroy')->middleware('can:program.user.destroy');
+Route::get('/programa/{flo?}/', 'App\Http\Controllers\Usuario\ProgramacionController@index')->middleware('can:program.user.edit');
+Route::get('/programa/{id}/edit', 'App\Http\Controllers\Usuario\ProgramacionController@edit')->middleware('can:program.user.edit');
+Route::put('/programa/{progra}/{acti?}', 'App\Http\Controllers\Usuario\ProgramacionController@update')->middleware('can:program.user.desactivar');
 
-Route::get('/progrflota/{insti}/{prog}/{tipo}', 'App\Http\Controllers\Usuario\ProgrFlotaController@index')->middleware('can:admin.role.index');
-Route::post('/progrflotaregist', 'App\Http\Controllers\Usuario\ProgrFlotaController@store')->middleware('can:admin.role.create');
-//Route::delete('/programa/{id}', 'App\Http\Controllers\Usuario\ProgrFlotaController@destroy')->middleware('can:admin.role.destroy');
-//Route::get('/progrflota/{flo?}/', 'App\Http\Controllers\Usuario\ProgrFlotaController@index')->middleware('can:admin.role.edit');
-//Route::get('/progrflota/{id}/edit', 'App\Http\Controllers\Usuario\ProgrFlotaController@edit')->middleware('can:admin.role.edit');
-//Route::put('/progrflota/{progra}/{acti?}', 'App\Http\Controllers\Usuario\ProgrFlotaController@update')->middleware('can:admin.role.edit');
+Route::get('/progrflota/{insti}/{prog}/{tipo}', 'App\Http\Controllers\Usuario\ProgrFlotaController@index')->middleware('can:proflo.user.index');
+Route::post('/progrflotaregist', 'App\Http\Controllers\Usuario\ProgrFlotaController@store')->middleware('can:proflo.user.create');
+//Route::delete('/programa/{id}', 'App\Http\Controllers\Usuario\ProgrFlotaController@destroy')->middleware('can:proflo.user.destroy');
+//Route::get('/progrflota/{flo?}/', 'App\Http\Controllers\Usuario\ProgrFlotaController@index')->middleware('can:proflo.user.edit');
+//Route::get('/progrflota/{id}/edit', 'App\Http\Controllers\Usuario\ProgrFlotaController@edit')->middleware('can:proflo.user.edit');
+//Route::put('/progrflota/{progra}/{acti?}', 'App\Http\Controllers\Usuario\ProgrFlotaController@update')->middleware('can:proflo.user.edit');
 
-Route::get('/areaser', 'App\Http\Controllers\Usuario\AreaServicioController@index')->middleware('can:admin.role.index');
+Route::get('/areaser', 'App\Http\Controllers\Usuario\AreaServicioController@index')->middleware('can:insti.user.index');
 //Route::post('/areaserregist', 'App\Http\Controllers\Usuario\AreaServicioController@store')->middleware('can:admin.role.create');
 //Route::delete('/areaser/{id}', 'App\Http\Controllers\Usuario\AreaServicioController@destroy')->middleware('can:admin.role.destroy');
 //Route::get('/areaser/{id}/edit', 'App\Http\Controllers\Usuario\AreaServicioController@edit')->middleware('can:admin.role.edit');
 //Route::put('/areaser/{areaser}', 'App\Http\Controllers\Usuario\AreaServicioController@update')->middleware('can:admin.role.edit');
 
-Route::get('/parroquia', 'App\Http\Controllers\Usuario\ParroquiaController@index')->middleware('can:admin.role.index');
+Route::get('/parroquia', 'App\Http\Controllers\Usuario\ParroquiaController@index')->middleware('can:insti.user.index');
 //Route::post('/parroquiaregist', 'App\Http\Controllers\Usuario\ParroquiaController@store')->middleware('can:admin.role.create');
 //Route::delete('/parroquia/{id}', 'App\Http\Controllers\Usuario\ParroquiaController@destroy')->middleware('can:admin.role.destroy');
 //Route::get('/parroquia/{id}/edit', 'App\Http\Controllers\Usuario\ParroquiaController@edit')->middleware('can:admin.role.edit');
